876eb0add4dd65f804feac3a1653efc67c50bfac
Cursos FIC <FIC@educ501.local>
2024-08-31 13:53:37 -0300

    arquivo sem o node

---
+++ src/app/pokemon/pokemon.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+import { PokemonComponent } from './pokemon.component';
+import { PokemonRoutingModule } from './pokemon-routing,modules';
+
+@NgModule({
+    imports: [
+        CommonModule,
+        FormsModule,
+        IonicModule,
+        PokemonRoutingModule
+    ],
+    declarations: [PokemonComponent]
+})
+export class PokemonComponentModule {}
